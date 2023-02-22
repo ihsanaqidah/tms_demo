59536370a51f1ae9913a575d3bdad143986c6a05
Ihsan Husnul <ihsan.husnul@gmail.com>
2023-02-22 12:11:53 +0700

    Initial Commit

---
+++ TMSDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMSDemo
+//
+//  Created by Ihsan Husnul Aqidah on 2/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
